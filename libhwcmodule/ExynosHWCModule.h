3dfe0253949373203a9965bca091691d8a6b4d4b
Christopher N. Hesse <raymanfx@gmail.com>
2016-02-24 23:11:09 +0100

    libhwcmodule: Add constants and functions for framebuffer abstraction
    Change-Id: I8aa264d79d1957d9c348e11e1eba03258836e1f7

--- libhwcmodule/ExynosHWCModule.h
+++ libhwcmodule/ExynosHWCModule.h
@@ -31,6 +31,51 @@
 
 #define DUAL_VIDEO_OVERLAY_SUPPORT
 
+typedef decon_win_config fb_win_config;
+typedef decon_win_config_data fb_win_config_data;
+
+const size_t SOC_NUM_HW_WINDOWS = 7;        // MAX_DECON_WIN
+const size_t SOC_WIN_STATE_DISABLED = 0;    // DECON_WIN_STATE_DISABLED
+const size_t SOC_WIN_STATE_COLOR = 1;       // DECON_WIN_STATE_COLOR
+const size_t SOC_WIN_STATE_BUFFER = 2;      // DECON_WIN_STATE_BUFFER
+const size_t SOC_BLENDING_MAX = 3;           // DECON_BLENDING_MAX
+const size_t SOC_PIXEL_FORMAT_MAX = 21;     // DECON_PIXEL_FORMAT_MAX
+
+inline size_t halBlendingToSocBlending(int32_t blending)
+{
+    switch (blending) {
+        case HWC_BLENDING_NONE:
+            return DECON_BLENDING_NONE;
+        case HWC_BLENDING_PREMULT:
+            return DECON_BLENDING_PREMULT;
+        case HWC_BLENDING_COVERAGE:
+            return DECON_BLENDING_COVERAGE;
+            
+        default:
+            return DECON_BLENDING_MAX;
+    }
+}
+
+inline size_t halFormatToSocFormat(int format)
+{
+    switch (format) {
+    case HAL_PIXEL_FORMAT_RGBA_8888:
+        return DECON_PIXEL_FORMAT_RGBA_8888;
+    case HAL_PIXEL_FORMAT_RGBX_8888:
+        return DECON_PIXEL_FORMAT_RGBX_8888;
+    case HAL_PIXEL_FORMAT_RGB_565:
+        return DECON_PIXEL_FORMAT_RGB_565;
+    case HAL_PIXEL_FORMAT_BGRA_8888:
+        return DECON_PIXEL_FORMAT_BGRA_8888;
+#ifdef EXYNOS_SUPPORT_BGRX_8888
+    case HAL_PIXEL_FORMAT_BGRX_8888:
+        return DECON_PIXEL_FORMAT_BGRX_8888;
+#endif
+    default:
+        return DECON_PIXEL_FORMAT_MAX;
+    }
+}
+
 #ifdef FIMD_BW_OVERLAP_CHECK
 const size_t MAX_NUM_FIMD_DMA_CH = 2;
 const uint32_t FIMD_DMA_CH_IDX[] = {0, 1, 1, 1, 0};
